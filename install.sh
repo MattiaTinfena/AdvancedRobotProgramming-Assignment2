7af27f5439244a6a3ab5d1af7bdf9379e782aae4
Mattia <mattia.tinfena@gmail.com>
2025-02-13 14:20:44 +0100

    install.sh updated

--- install.sh
+++ install.sh
@@ -9,12 +9,20 @@ sudo apt install konsole
 sudo apt install libncurses-dev
 sudo apt install libcjson-dev
 
+echo "Please specify an installation folder:"
+read install_folder
+
+if [ ! -d "$install_folder" ]; then
+  echo "Error: the specified installation folder does not exist."
+  exit 1
+fi
+
+cd "$install_folder"
+mkdir -p fastdds
+
 curl -o fastdds.tgz 'https://www.eprosima.com/component/ars/item/eProsima_Fast-DDS-v3.1.0-Linux.tgz?format=tgz&category_id=7&release_id=169&Itemid=0'
 
-mkdir fastdds
-tar -xvzf ./fastdds.tgz -C ./fastdds
-cd fastdds
+tar -xvzf ./fastdds.tgz -C fastdds
+cd fastdds/fastdds
 sudo ./install.sh
 
-make clean
-make 
